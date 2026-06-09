4d9de5a4481af9239740d004a3a6af646662449a
Jonnathan <jxav_91@hotmail.com>
2026-06-09 10:51:26 -0500

    probar api football mundial

--- app/api/importar-mundial/route.ts
+++ app/api/importar-mundial/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   const apiKey = process.env.API_FOOTBALL_KEY
 
   const response = await fetch(
-    'https://v3.football.api-sports.io/leagues',
+    'https://v3.football.api-sports.io/leagues?id=1',
     {
       headers: {
         'x-apisports-key': apiKey || ''
